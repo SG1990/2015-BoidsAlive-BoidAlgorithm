6d4ae770b670714083365e36c1d7bd5a76a4ea31
Aleksandra Korach <sgsbox90@gmail.com>
2015-04-13 01:57:07 +0200

    commit correction

--- BoidsAlive/src/Boid.java
+++ BoidsAlive/src/Boid.java
@@ -16,7 +16,7 @@ public class Boid extends WorldObject{
 		graphicEle.refreshLocation();
 		
 		try {
-			Thread.sleep(10);
+			sleep(10);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
