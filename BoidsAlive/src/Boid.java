f564bf82c438eab338e721192290405c6297fe9c
Michal Krolikowski <hiszpanskainkwizycja@gmail.com>
2015-05-10 23:23:29 +0200

    Proper angle calculation

--- BoidsAlive/src/Boid.java
+++ BoidsAlive/src/Boid.java
@@ -161,11 +161,10 @@ public class Boid extends WorldObject {
 	}
 	
 	private double getAngleBetween(double x1, double y1, double x2, double y2) {		
-		double aob = (x1 * x2) + (y1 * y2);
-		double axb = (Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2))) * 
-				(Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2)));
-		
-		return Math.acos(aob/axb);
+		return Math.abs(
+				Math.toDegrees(Math.atan2(y1,x1)) - 
+				Math.toDegrees(Math.atan2(y2,x2))
+			);
 	}
 	
 	private void checkVelocity() {
